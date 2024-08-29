634562c58dadd8e6c64d5a9d9d4885872a24a2ac
viragtiwari <viragtiwari200424@gmail.com>
2024-08-30 02:30:09 +0530

    Continuous chatflow

--- complaint_register.py
+++ complaint_register.py
@@ -81,22 +81,27 @@ while True:
     print(response)
     if response.strip() == "Your complaint has been registered successfully.":
         response = complaint_registration("Summary")
-        break
+        pnr = extract_pnr(response)
+        problem = extract_problem(response)
+        sentiment = extract_sentiment(response)
+        # workflow
+        complaint_number = generate_special_string(pnr)
+        print(complaint_number)  # testing
+        route = complaint_route(pnr, problem, sentiment)
+        print(route)
+        while True:
+            query1 = input("Message:  ")
+            response1 = complaint_registration(f"""
+Complaint has been registered with Complaint number: {complaint_number}
+Now your task is to assure user their problem would be resolved as soon as possible
+Answer the query in the most empathetic manner
 
+Query:
+{query1}""")
+            print(response1)
 
-# chat ends here
-pnr = extract_pnr(response)
-problem = extract_problem(response)
-sentiment = extract_sentiment(response)
+    
 
-# workflow
-complaint_number = generate_special_string(pnr)
-print(complaint_number)  # testing
-
-
-route = complaint_route(pnr, problem, sentiment)
-
-print(route)
 
 # Urgency codes comes here::::
 # ======== code ========
