cc03fc95f7d9fb27a95e3baecf367a4d730a8c1e
viragtiwari <viragtiwari200424@gmail.com>
2024-09-04 14:09:13 +0530

    Migrated to claude and added multimodality

--- smart_routing.py
+++ smart_routing.py
@@ -1,16 +1,18 @@
 from model.model import routing_model
 import utils
+from model.prompts import ROUTE_PROMPT
 
-chat_session = routing_model.start_chat(history=[])
 
+conversation = []
+
+def update_conversation(role, content):
+    conversation.append({"role": role, "content": content})
+
+systemprompt = ROUTE_PROMPT
 
 def get_route(summary):
-    
     pnr, problem, sentiment = utils.parse_summary(summary)
-    
-    response = chat_session.send_message(
-        f"Pnr: {pnr} :: Problem: {problem} :: Sentiment of the user: {sentiment}"
-    )
-
+    update_conversation("user", f"Pnr: {pnr} :: Problem: {problem} :: Sentiment of the user: {sentiment}")
+    response = routing_model(conversation,systemprompt)
     ucid = utils.generate_ucid(pnr_number=pnr)
     return response.text, ucid
